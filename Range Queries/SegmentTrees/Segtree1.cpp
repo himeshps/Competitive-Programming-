5bb0743c1160fbec716d369ccb40b11ae2dddeeb
semi__h <152438086+himeshps@users.noreply.github.com>
2025-01-13 12:52:28 +0530

    updated seg trees code for correction

--- Range Queries/SegmentTrees/Segtree1.cpp
+++ Range Queries/SegmentTrees/Segtree1.cpp
@@ -1,6 +1,5 @@
- 
 class SegmentTree {
-    vector<int> tree;
+    vector<long long int> tree;
     int n;
  
 public:
@@ -17,18 +16,18 @@ public:
             int mid = (start + end) / 2;
             build(a, 2 * node + 1, start, mid);
             build(a, 2 * node + 2, mid + 1, end);
-            tree[node] = tree[2 * node + 1] & tree[2 * node + 2];
+            tree[node] =(tree[2 * node + 1] ^ tree[2 * node + 2]);
         }
     }
  
-    int query(int l, int r, int node, int start, int end) {
-        if (r < start || l > end) return INT_MAX; // Neutral for AND
+    long long int query(int l, int r, int node, int start, int end) {
+        if (r < start || l > end) return 0; // Neutral for AND
         if (l <= start && end <= r) return tree[node];
-        int mid = (start + end) / 2;
-        return query(l, r, 2 * node + 1, start, mid) & query(l, r, 2 * node + 2, mid + 1, end);
+        long long int mid = (start + end) / 2;
+        return (query(l, r, 2 * node + 1, start, mid) ^ query(l, r, 2 * node + 2, mid + 1, end));
     }
  
-    int query(int l, int r) {
+    long long int query(int l, int r) {
         return query(l, r, 0, 0, n - 1);
     }
-}
+};
