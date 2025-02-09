126e1e0e8effffa0a6703a73929c8e0962cd8204
semi__h <152438086+himeshps@users.noreply.github.com>
2025-02-09 19:25:21 +0530

    added the bellman ford code to the repo lessgooo !!

--- Graphs/Bellman_Ford.cpp
+++ Graphs/Bellman_Ford.cpp
@@ -1,1 +1,43 @@
+/* This function keeps the track of the shortest distance array d as well has helps detect the negativce edge cycle and can also print it . 
+  */
+void BellmanFord()
+{
+    vector<int> d(n, INF);
+    d[v] = 0;
+    vector<int> p(n, -1);
+    int x;
+    for (int i = 0; i < n; ++i) {
+        x = -1;
+        for (Edge e : edges)
+            if (d[e.a] < INF)
+                if (d[e.b] > d[e.a] + e.cost) {
+                    d[e.b] = max(-INF, d[e.a] + e.cost);
+                    p[e.b] = e.a;
+                    x = e.b;
+                }
+    }
 
+    if (x == -1)
+        cout << "No negative cycle from " << v;
+    else {
+        int y = x;
+        for (int i = 0; i < n; ++i)
+            y = p[y];
+
+        vector<int> path;
+        for (int cur = y;; cur = p[cur]) {
+            path.push_back(cur);
+            if (cur == y && path.size() > 1)
+                break;
+        }
+        reverse(path.begin(), path.end());
+
+        cout << "Negative cycle: ";
+        for (int u : path)
+            cout << u << ' ';
+    }
+}
+
+/*
+Make the required changes in the code with the name of the data structures and the keywords. That is important and rest is easy afai see !!!
+*/
